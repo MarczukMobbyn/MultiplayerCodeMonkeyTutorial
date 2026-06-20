178b75133d2bddb6920856c30c00404eeab69a05
MarczukMobbyn <284281@student.pwr.edu.pl>
2026-06-21 01:35:34 +0200

    basic multiplayer setup

---
+++ Assets/Scripts/NetworkManager/NetworkManagerUI.cs
@@ -0,0 +1,27 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button clientBtn;
+    [SerializeField] private Button hostBtn;
+
+
+    private void Awake()
+    {
+        serverBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+        hostBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        clientBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
