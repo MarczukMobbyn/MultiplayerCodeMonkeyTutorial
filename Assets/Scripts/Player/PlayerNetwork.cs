cf29ef668ae0a8141f4531a102823cba1972d762
MarczukMobbyn <284281@student.pwr.edu.pl>
2026-06-21 19:35:12 +0200

    sth

--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -1,13 +1,60 @@
+using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    
+    [SerializeField] Transform spawnedObjectPrefab;
+
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(new MyCustomData
+    {
+        id = 56,
+        _bool = true,
+    },NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int id;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref id);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; " + newValue.id + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
+
     void Update()
     {
+   
         if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Transform spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+
+            //TestClientRpc(new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new List<ulong> { 1 } } });
+
+            //randomNumber.Value = new MyCustomData
+            //{
+            //    id = Random.Range(0, 100),
+            //    _bool = Random.Range(0, 2) == 0 ? true : false,
+            //    message = "gay boom"
+            //};
+        }
 
         Vector3 moveDir = new Vector3(0, 0, 0);
 
@@ -18,5 +65,17 @@ public class PlayerNetwork : NetworkBehaviour
 
         float moveSpeed = 30f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    [ServerRpc]
+    private void TestServerRpc(ServerRpcParams serverRpcParams)
+    {
+        Debug.Log("TestServerRpc " + OwnerClientId + "; " + serverRpcParams.Receive.SenderClientId);
+    }
+
+    [ClientRpc]
+    private void TestClientRpc(ClientRpcParams clientRpcParams)
+    {
+        Debug.Log("TestClientRpc");
     }
 }
